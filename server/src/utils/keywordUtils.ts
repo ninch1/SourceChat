16290d31e3daaccf6a057e308e660c02af1b3c8b
Elguja Modebadze <gujiko.modebadze@gmail.com>
2026-06-21 01:10:20 +0400

    Improve keyword extraction

--- server/src/utils/keywordUtils.ts
+++ server/src/utils/keywordUtils.ts
@@ -1,12 +1,40 @@
 // helper function to extract keywords from a text
 
+const STOP_WORDS = new Set([
+  "a",
+  "an",
+  "and",
+  "are",
+  "as",
+  "at",
+  "be",
+  "by",
+  "can",
+  "do",
+  "does",
+  "for",
+  "from",
+  "have",
+  "how",
+  "in",
+  "is",
+  "it",
+  "of",
+  "on",
+  "or",
+  "should",
+  "the",
+  "to",
+  "with",
+  "work",
+]);
+
 export const extractKeywords = (text: string): string[] => {
-  return Array.from(
-    new Set(
-      text
-        .toLowerCase()
-        .split(/\W+/)
-        .filter((word) => word.length > 2), // removes small words like "a", "an", "to"
-    ),
-  );
+  const words = text
+    .toLowerCase()
+    .split(/\W+/)
+    .filter((word) => word.length > 2) // removes small words like "a", "an", "to"
+    .filter((word) => !STOP_WORDS.has(word)); // removes stop words
+
+  return [...new Set(words)]; // removes duplicates by converting to a set
 };
