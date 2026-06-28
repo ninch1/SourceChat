ece10c63138830aa76af579d87c1afc0070fdbff
Elguja Modebadze <gujiko.modebadze@gmail.com>
2026-06-28 23:29:29 +0400

    Set up frontend routing and query provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
-import HeroPage from "./pages/HeroPage";
+import { Route, Routes } from 'react-router-dom';
+import HeroPage from './pages/HeroPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
 
 export default function App() {
   return (
-    <div>
-      <HeroPage />
-    </div>
+    <Routes>
+      <Route path='/' element={<HeroPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+      <Route path='/dashboard' element={<DashboardPage />} />
+    </Routes>
   );
 }
