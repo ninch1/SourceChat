17d1726a6da9a4081ecd1f03755882417e96eaaa
Elguja Modebadze <gujiko.modebadze@gmail.com>
2026-07-14 19:47:12 +0400

    Prepare cookies for cross-site deployment

--- server/src/utils/authUtils.ts
+++ server/src/utils/authUtils.ts
@@ -1,14 +1,14 @@
-import type { Request } from 'express';
-import ErrorResponse from '../errors/errorResponse.js';
-import jwt from 'jsonwebtoken';
-import crypto from 'crypto';
+import type { Request } from "express";
+import ErrorResponse from "../errors/errorResponse.js";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
 if (!ACCESS_TOKEN_SECRET || !REFRESH_TOKEN_SECRET) {
-  console.error('Access token secret or refresh token secret is not set');
-  throw new ErrorResponse('Internal server error', 500);
+  console.error("Access token secret or refresh token secret is not set");
+  throw new ErrorResponse("Internal server error", 500);
 }
 
 type TokenPayload = {
@@ -17,18 +17,18 @@ type TokenPayload = {
 
 export const refreshTokenCookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: (process.env.NODE_ENV === 'production' ? 'strict' : 'lax') as
-    | 'strict'
-    | 'lax',
-  path: '/',
+  sameSite: (process.env.NODE_ENV === "production" ? "none" : "lax") as
+    | "none"
+    | "lax",
+  secure: process.env.NODE_ENV === "production",
+  path: "/",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
 // Get the authenticated user from the request
 export const getAuthUser = (req: Request) => {
   if (!req.user) {
-    throw new ErrorResponse('Unauthorized', 401);
+    throw new ErrorResponse("Unauthorized", 401);
   }
 
   return req.user;
@@ -36,18 +36,18 @@ export const getAuthUser = (req: Request) => {
 
 export const generateAccessToken = (payload: TokenPayload) => {
   return jwt.sign(payload, ACCESS_TOKEN_SECRET, {
-    expiresIn: '15m',
+    expiresIn: "15m",
   });
 };
 
 export const generateRefreshToken = (payload: TokenPayload) => {
   return jwt.sign(payload, REFRESH_TOKEN_SECRET, {
-    expiresIn: '7d',
+    expiresIn: "7d",
   });
 };
 
 export const hashRefreshToken = (token: string): string => {
-  return crypto.createHash('sha256').update(token).digest('hex');
+  return crypto.createHash("sha256").update(token).digest("hex");
 };
 
 export const verifyAccessToken = (token: string): TokenPayload => {
